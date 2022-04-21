d52adefe2d75c9b68805dea42bf38d5ce5a07d8c
Yuhang Zhou <69432560+yuhang199@users.noreply.github.com>
2022-04-21 16:51:16 -0400

    6th

--- guess-flip.js
+++ guess-flip.js
@@ -3,7 +3,7 @@ import minimist from 'minimist'
 
 const arg = minimist(process.argv.slice(2))
 arg["call"]
-if (arg.call == undefined || arg.call != "heads" || arg.call != "tails") {
+if (arg.call == undefined || (arg.call != "heads" && arg.call != "tails")) {
     console.log("Error: no input.")
     console.log("Usage: node guess-flip --call=[heads|tails]")
 }else{
